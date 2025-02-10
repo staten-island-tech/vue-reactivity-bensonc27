193525aadba4a1580d9c53bb1589df4870e0591c
bensonc27 <bensonc27@nycstudents.net>
2025-02-10 12:30:59 -0500

    dafdsafdas

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,16 +1,79 @@
 <script setup>
-import { products } from './components/products.js'
+import { ref } from 'vue'
+import Card from './components/Items.vue'
+const products = ref([
+  {
+    name: 'Berry Cone',
+    src: '/berrycone.png',
+    cost: 4,
+  },
+  {
+    name: 'Chocolate Bar',
+    src: '/chocolatebar.png',
+    cost: 3.5,
+  },
+  {
+    name: 'ChocoChip Sandwich',
+    src: '/chocolatechipsandwich.png',
+    cost: 2.5,
+  },
+  {
+    name: 'Chocolate Cone',
+    src: '/chocolatecone.png',
+    cost: 2.5,
+  },
+  {
+    name: 'Cookie',
+    src: '/cookie.png',
+    cost: 2.0,
+  },
+  {
+    name: 'Mint Chocolate Cone',
+    src: '/mintchoco.png',
+    cost: 4.0,
+  },
+  {
+    name: 'Mixed Cone',
+    src: '/mixcone.png',
+    cost: 4.5,
+  },
+  {
+    name: 'Quadruple Scoop',
+    src: '/quadruplescoop.png',
+    cost: 5.0,
+  },
+  {
+    name: 'Strawberry Cone',
+    src: '/strawberrycone.png',
+    cost: 3.5,
+  },
+  {
+    name: 'Twix Bar',
+    src: '/twixbar.png',
+    cost: 2.5,
+  },
+  {
+    name: 'Vanilla Cone',
+    src: '/vanillacone.png',
+    cost: 3.5,
+  },
+  {
+    name: 'Waffle',
+    src: '/waffle.png',
+    cost: 4.5,
+  },
+])
 </script>
 
 <template>
-  <h1>Ice Cream Shop</h1>
-  <div id="store">
-    <icecream-item
-      v-for="product in products"
-      :icecream-name="product.name"
-      :icecream-price="product.cost"
-    />
+  <div>
+    <Card v-for="product in products" :key="product.name" :product="product" />
   </div>
 </template>
 
-<style></style>
+<style>
+.product-image {
+  width: 30%;
+  height: auto;
+}
+</style>
--- app/src/components/Items.vue
+++ app/src/components/Items.vue
@@ -1,12 +1,19 @@
 <template>
   <div>
     <h2>
-      {{ foodName }}
+      {{ product.name }}
     </h2>
-    <p></p>
+    <p>${{ product.cost }}</p>
+    <img :src="product.src" class="product-image" />
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+defineProps({
+  product: {
+    type: Object,
+  },
+})
+</script>
 
 <style lang="scss" scoped></style>
